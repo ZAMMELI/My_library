10910521ce6bd69f7c9ebca746c9d1a66152de0c
ZAMMELI <aymen.01.zammeli@gmail.com>
2024-12-05 22:24:46 +0100

    this is my first commit

---
+++ src/test/java/com/my_library/MyLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
